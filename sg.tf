de321d024249b9ad049a34f27a2fce5de694a822
bhrakesh <rakeshlinux3@gmail.com>
2024-12-30 14:41:03 +0530

    Update sg.tf
    removed unwanted lines

--- sg.tf
+++ sg.tf
@@ -6,20 +6,20 @@ resource "aws_security_group" "lseg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Open SSH access (adjust for security)
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Open HTTP access (adjust for security)
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1" # All outbound traffic
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
